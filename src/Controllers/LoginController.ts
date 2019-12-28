e1fee90d4e0c0b7aeeae54308f77cac096cf295f
Andrew Parnham <atparnham@gmail.com>
2019-12-28 14:24:57 +0000

    PoC using decorators and metadata

---
+++ src/Controllers/LoginController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { get } from "./Decorators/routes";
+import { controller } from "./Decorators/controller";
+
+@controller("/auth")
+class LoginController {
+  @get("/login")
+  getLogin(req: Request, res: Response): void {
+    res.send(`
+      <form method="POST">
+        <div>
+          <label>Email</label>
+          <input name="email" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input name="password" type="password" />
+        </div>
+        <button>Submit</button>
+      </form>
+    `);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express, { Request, Response } from "express";
 import { router } from "./Routes/loginRoutes";
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
+import "./Controllers/LoginController";
+import { router as controllerRouter } from "./Controllers/Decorators/controller";
 
 const app = express();
 
 app.use(cookieSession({ keys: ["someRandomKey"] }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
