9a8f5fdb861bb803ff169f4abe56a60fa40e37f9
Andrew Parnham <atparnham@gmail.com>
2019-12-28 15:31:02 +0000

    Remove test code

--- src/Controllers/LoginController.ts
+++ src/Controllers/LoginController.ts
@@ -1,20 +1,11 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import { get, post } from "./Decorators/routes";
 import { controller } from "./Decorators/controller";
-import { use } from "./Decorators/use";
 import { bodyValidator } from "./Decorators/bodyValidator";
-
-//Test middleware
-function logger(req: Request, res: Response, next: NextFunction) {
-  console.log("Request made");
-
-  next();
-}
 
 @controller("/auth")
 class LoginController {
   @get("/login")
-  @use(logger)
   getLogin(req: Request, res: Response): void {
     res.send(`
       <form method="POST">
