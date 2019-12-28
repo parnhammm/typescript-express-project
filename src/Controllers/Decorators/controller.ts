d604dce04141695eb48d94ee73343642284e744e
Andrew Parnham <atparnham@gmail.com>
2019-12-28 15:16:25 +0000

    Adding a body validator middleware

--- src/Controllers/Decorators/controller.ts
+++ src/Controllers/Decorators/controller.ts
@@ -2,6 +2,26 @@ import "reflect-metadata";
 import { AppRouter } from "../../AppRouter";
 import { Methods } from "./Enums/Methods";
 import { MetadataKeys } from "./Enums/MetadataKeys";
+import { Request, Response, RequestHandler, NextFunction } from "express";
+
+//Create a body validator middleware!
+function bodyValidators(keys: string[]): RequestHandler {
+  return function(req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      res.status(422).send("Invalid request");
+      return;
+    }
+
+    for (let key of keys) {
+      if (!req.body[key]) {
+        res.status(422).send("Invalid request");
+        return;
+      }
+    }
+
+    next();
+  };
+}
 
 export function controller(routePrefix: string) {
   return function(target: Function) {
@@ -9,7 +29,7 @@ export function controller(routePrefix: string) {
 
     for (let key in target.prototype) {
       const handler = target.prototype[key];
-      
+
       const path = Reflect.getMetadata(
         MetadataKeys.Path,
         target.prototype,
@@ -26,8 +46,19 @@ export function controller(routePrefix: string) {
         Reflect.getMetadata(MetadataKeys.Middleware, target.prototype, key) ||
         [];
 
+      const requiredBodyProps =
+        Reflect.getMetadata(MetadataKeys.Validator, target.prototype, key) ||
+        [];
+
+      const validator = bodyValidators(requiredBodyProps);
+
       if (path) {
-        router[method](`${routePrefix}${path}`, ...middlewares, handler);
+        router[method](
+          `${routePrefix}${path}`,
+          ...middlewares,
+          validator,
+          handler
+        );
       }
     }
   };
